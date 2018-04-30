4d119c0b8536c2875cb45f510b66618ab321b6f3
Chanel Rodriguez <13car92@gmail.com>
2018-04-30 09:24:57 -0500

    first commit

---
+++ src/main/java/com/codeup/habitperformancegame/HabitPerformanceGameApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.habitperformancegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitPerformanceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitPerformanceGameApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codeup/habitperformancegame/HabitPerformanceGameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeup.habitperformancegame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HabitPerformanceGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
