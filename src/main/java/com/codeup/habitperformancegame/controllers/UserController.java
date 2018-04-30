069bcb5d29a0047f15ed052854b2df7ea87cb66c
zacharyramos <zachary.ramos9@gmail.com>
2018-04-30 14:52:29 -0500

    post mapping for saving new user info

--- src/main/java/com/codeup/habitperformancegame/controllers/UserController.java
+++ src/main/java/com/codeup/habitperformancegame/controllers/UserController.java
@@ -6,21 +6,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
     private UserRepository userDao;
-    private PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;// to be used when implimenting security
 
-    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder){
+    public UserController(UserRepository userDao){
         this.userDao = userDao;
-        this.passwordEncoder = passwordEncoder;
+
     }
 
+    //get register page
     @GetMapping("/register")
     public String showRegisterForm(Model model){
         model.addAttribute("user", new User());
         return "users/register";
+    }
+
+    //save user info
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+        String password = user.getPassword();
+        user.setPassword(password);
+        userDao.save(user);
+        return "redirect:/login";
     }
 
 
